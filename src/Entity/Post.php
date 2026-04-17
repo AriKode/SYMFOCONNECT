ef15d74dad3b15dd7704ba82a8fa1eac7ac7467c
Arièle <ariele.ratsimbazafy@gmail.com>
2026-04-17 10:10:48 +0200

    Update

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource()]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
